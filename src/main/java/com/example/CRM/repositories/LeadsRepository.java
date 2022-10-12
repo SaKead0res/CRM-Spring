418f54fceb45f5b66155e0f3fbc8d97b15eb8432
Max Bonas <113984950+MaxBonas@users.noreply.github.com>
2022-10-12 12:36:52 +0200

    Refactor fixed

--- src/main/java/com/example/CRM/repositories/LeadsRepository.java
+++ src/main/java/com/example/CRM/repositories/LeadsRepository.java
@@ -1,6 +1,6 @@
 package com.example.CRM.repositories;
 
-import com.example.CRM.entities.Lead;
+import com.example.CRM.entities.Leads;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,9 +8,9 @@ import java.util.*;
 
 
 @Repository
-public interface LeadRepository extends JpaRepository<Lead, Long> {
+public interface LeadsRepository extends JpaRepository<Leads, Long> {
 
-    List<Lead> findByName (String name);
+    List<Leads> findByName (String name);
 
 //    Optional<Leads> getById(Long id);
 }
